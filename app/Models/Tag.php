acb6f1accc94dec26fbafb9a7f02011fdbb9291b
Djiatsa Dunamis Junior <dunamisjunior15@gmail.com>
2024-11-23 10:55:36 +0100

    testing tuto complete.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\TagFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    /** @use HasFactory<\Database\Factories\TagFactory> */
+    /** @use HasFactory<TagFactory> */
     use HasFactory;
+
+    protected $fillable = ['name'];
 }
