df57cf9a8c529b1fa7ad8da8c82ced38c9e718c7
Djiatsa Dunamis Junior <dunamisjunior15@gmail.com>
2024-11-23 10:14:08 +0100

    employer migration.

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,15 @@
-<a href="#" class="bg-white/20 hover:bg-white/50 px-3 py-1 rounded-xl text-2xs font-bold transition-colors 300">Tag</a>
+@props(['size' => 'base'])
+
+@php
+    $classes = "bg-white/20 hover:bg-white/50 rounded-xl font-bold transition-colors 300";
+
+    if($size == 'base') {
+        $classes .= " px-5 py-1 text-sm";
+    }
+
+    if($size == 'small') {
+        $classes .= " px-3 py-1 text-2xs";
+    }
+@endphp
+
+<a href="#" class="{{ $classes }}">{{ $slot }}</a>
